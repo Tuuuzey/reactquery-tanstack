f6de3576f191d80e10a94fafcc4f4aa070d9530d
Milosz Wach <wach.milosz1@gmail.com>
2025-06-19 12:59:43 +0200

    .

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,12 +1,35 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import { useQuery, useMutation } from '@tanstack/react-query';
+import { fetchEvent, updateEvent, queryClient } from '../../util/http.js';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
+import LoadingIndicator from '../UI/LoadingIndicator.jsx'
 
 export default function EditEvent() {
   const navigate = useNavigate();
+  const { id } = useParams();
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['events', id],
+    queryFn: ({signal})=> fetchEvent({signal, id})
+  })
 
-  function handleSubmit(formData) {}
+  const { mutate } = useMutation({
+    mutationFn: updateEvent,
+    onMutate: async (data) => {
+      const newEvent = data.event
+      await queryClient.cancelQueries({queryKey: ['events', id]})
+      queryClient.setQueryData(['events', id], newEvent);
+    },
+  });
+
+  function handleSubmit(formData) {
+    mutate({
+      id, event: formData
+    })
+    navigate('../');
+  }
 
   function handleClose() {
     navigate('../');
@@ -14,14 +37,25 @@ export default function EditEvent() {
 
   return (
     <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
-        <Link to="../" className="button-text">
-          Cancel
-        </Link>
-        <button type="submit" className="button">
-          Update
-        </button>
-      </EventForm>
+      {isError && (
+        <>
+          <ErrorBlock title={'Failed to edit'} message={error.info?.message || 'Please try again later'} />
+          <Link to={'../'} className='button' />
+        </>
+      )}
+      {isPending && (
+        <LoadingIndicator />
+      )}
+      {!isError && !isPending && (
+        <EventForm inputData={data} onSubmit={handleSubmit}>
+          <Link to="../" className="button-text">
+            Cancel
+          </Link>
+          <button type="submit" className="button">
+            Update
+          </button>
+        </EventForm>
+      )}
     </Modal>
   );
 }
--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,35 +1,96 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useParams, useNavigate } from 'react-router-dom';
 
 import Header from '../Header.jsx';
+import { useQuery, useMutation } from '@tanstack/react-query';
+import { fetchEvent, deleteEvent, queryClient } from '../../util/http.js';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
+import { useState } from 'react';
+import Modal from '../UI/Modal.jsx'
 
 export default function EventDetails() {
+
+  const [ isDeleting, setIsDeleting ] = useState(false);
+
+  const navigate = useNavigate()
+
+  const { id } = useParams();
+  const { data, isPending, isError, error } = useQuery({
+    queryFn: ({ signal }) => fetchEvent({ signal, id }),
+    queryKey: ['event', id],
+  });
+
+  const {mutate, isPending: isPendingDelete, isError: isErrorDelete, error: errorDelete} = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: ()=> {
+      queryClient.invalidateQueries({
+        queryKey: ['events'],
+        refetchType: 'none'
+      })
+      navigate('/events');
+    }
+  }) 
+
+  function handleStartDelete() {
+    setIsDeleting(true)
+  }
+
+  function handleStopDelete() {
+    setIsDeleting(false)
+  }
+
+  function handleDelete() {
+    mutate({id: id});
+  }
+  
+  if(isError) {
+    <ErrorBlock title={'Failed to load event'} message={error.info?.message || 'Try again later'}/>
+  }
+
   return (
     <>
+    {isDeleting && (
+      <Modal onClose={handleStopDelete}>
+        <h2>Are you sure you want to delete this event?</h2>
+        <div className='form-actions'>
+        {isPendingDelete && <p>Deleting please wait...</p>}
+        {!isPendingDelete && (
+          <>
+            <button onClick={handleDelete} className='button'>Delete</button>
+            <button onClick={handleStopDelete} className='button-text'>Cancel</button>
+          </>
+        )}
+        </div>
+        {isErrorDelete && <ErrorBlock title={'Failed to delete event'} message={errorDelete.info?.message ||'Please try again later'}/>}
+      </Modal>
+    )}
       <Outlet />
       <Header>
         <Link to="/events" className="nav-item">
           View all Events
         </Link>
       </Header>
-      <article id="event-details">
-        <header>
-          <h1>EVENT TITLE</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-        <div id="event-details-content">
-          <img src="" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
+      {isPending && <p>Fetching event...</p>}
+      {!isPending && (
+        <article id="event-details">
+          <header>
+            <h1>{data.title}</h1>
+            <nav>
+              <button onClick={handleStartDelete}>Delete</button>
+              <Link to="edit">Edit</Link>
+            </nav>
+          </header>
+          <div id="event-details-content">
+            <img src={`http://localhost:3000/${data.image}`} alt="" />
+            <div id="event-details-info">
+              <div>
+                <p id="event-details-location">{data.location}</p>
+                <time dateTime={`Todo-DateT$Todo-Time`}>{data.date} {data.time}</time>
+              </div>
+              <p id="event-details-description">{data.description}</p>
             </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
           </div>
-        </div>
       </article>
+      )}
     </>
   );
 }
